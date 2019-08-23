e88dab578ba568f1e250a37367e6e50272d752e4
James Linnegar <jlinnega@gmail.com>
2019-08-23 15:50:55 +0100

    testing palette

--- test/palette_test.py
+++ test/palette_test.py
@@ -1,52 +1,47 @@
-import src.color
-import src.palette
+from src.color import Color
+from src.palette import Palette
 
 
-
-class TestPaletter:
-    def test_something(self):
+class TestPalette:
+    def test_default_palette(self):
         now = 0
-        black = src.color.Color(0,0,0)
-        p = src.palette.Palette()
+        black = Color(0,0,0)
+        p = Palette()
         current_color = p.color_to_render(now)
         assert current_color.instruction() == black.instruction()
         
-
-    
-'''
     def test_before_animation_starts(self):
+        p = Palette()
         now = 0
         animation_length = 10
         animation_start_time = 5  
-        previous_color = Color(255,0,0)
-        current_color = Color(255,0,0)
         target_color = Color(0,0,255)
-        
-        calculated_color = calculate_color(now, animation_length, animation_start_time, previous_color, current_color, target_color)
-        assert calculated_color == current_color
+        p.update(target_color, animation_start_time, animation_length)
+
+        calculated_color = p.color_to_render(now)
+        assert calculated_color.instruction() == Color(0,0,0).instruction()
+
 
     def test_mid_animation(self):
+        p = Palette()
         now = 5
         animation_length = 10
         animation_start_time = 0  
-        previous_color = Color(255,0,0)
-        current_color = Color(255,0,0)
         target_color = Color(0,0,255)
-        
-        calculated_color = calculate_color(now, animation_length, animation_start_time, previous_color, current_color, target_color)
-        assert calculated_color.r == 127
+        p.update(target_color, animation_start_time, animation_length)
+        calculated_color = p.color_to_render(now)
+        assert calculated_color.r == 0
         assert calculated_color.g == 0
         assert calculated_color.b == 127
-        
-
-    def test_before_animation_starts(self):
+    
+    def test_post_animation(self):
+        p = Palette()
         now = 16
         animation_length = 10
         animation_start_time = 5  
-        previous_color = Color(255,0,0)
-        current_color = Color(255,0,0)
         target_color = Color(0,0,255)
-        
-        calculated_color = calculate_color(now, animation_length, animation_start_time, previous_color, current_color, target_color)
-        assert calculated_color == target_color
-    '''
+        p.update(target_color, animation_start_time, animation_length)
+        calculated_color = p.color_to_render(now)
+        assert calculated_color.r == 0
+        assert calculated_color.g == 0
+        assert calculated_color.b == 255

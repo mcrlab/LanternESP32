56d027ce5402fae0d2c9fd1e663e78530369119a
James Linnegar <jlinnega@gmail.com>
2019-08-23 18:20:09 +0100

    More colour tests

--- test/color_test.py
+++ test/color_test.py
@@ -15,17 +15,17 @@ class TestClassColor:
         c = color.Color(255, 0, 255)
         assert c.as_object() == {"r": 255, "g": 0, "b": 255}
 
-    def test_normalise_should_not_affect_color_less_than_target(self):
+    def test_normalise_should_not_affect_color_less_than_target_single_color(self):
         c = color.Color(255,0,0)
         c.normalise(512)
         assert c.as_object() == { "r": 255, "g": 0, "b": 0}
 
-    def test_normalise_should_not_affect_color_less_than_target(self):
+    def test_normalise_should_not_affect_color_less_than_target_multi_color(self):
         c = color.Color(255,255,0)
         c.normalise(512)
         assert c.as_object() == { "r": 255, "g": 255, "b": 0}
 
-    def test_normalise_should_not_affect_color_less_than_target(self):
+    def test_normalise_should_affect_color_greater_than_target(self):
         c = color.Color(255,255,255)
         c.normalise(512)
         assert c.as_object() == { "r": 170, "g": 170, "b": 170}

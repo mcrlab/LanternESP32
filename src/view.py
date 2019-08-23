a21afc93f079ed4aa140977900226ec025282b9c
James Linnegar <jlinnega@gmail.com>
2019-08-23 16:54:22 +0100

    Adding in fakes for neopixel and testing

--- src/view.py
+++ src/view.py
@@ -3,8 +3,7 @@ from neopixel import NeoPixel
 class View():
     def __init__(self, pin, number_of_pixels):
         self.number_of_pixels = number_of_pixels
-        self.pin = pin 
-        self.np = NeoPixel(self.pin, self.number_of_pixels)  
+        self.np = NeoPixel(pin, self.number_of_pixels)  
         self.last_render_time = 0
         
     def render(self, color, current_time):
---
+++ test/view_test.py
@@ -0,0 +1,23 @@
+import os, sys
+fake_dir = os.path.join(os.path.dirname(__file__), 'fake')
+print(fake_dir)
+assert(os.path.exists(fake_dir))
+sys.path.insert(0, fake_dir)
+
+from src.view import View
+from src.color import Color
+
+class TestClassView():
+    def test_intialisation(self):
+        pin = 0
+        number_of_pixels = 16
+        v = View(pin, number_of_pixels)
+        assert v.number_of_pixels == number_of_pixels
+
+    def test_render(self):
+        current_time = 15
+        pin = 0
+        number_of_pixels = 16
+        v = View(pin, number_of_pixels)
+        v.render(Color(0,0,0), current_time)
+        assert v.last_render_time == current_time     

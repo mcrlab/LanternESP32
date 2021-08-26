323aa7509b62ff2e10b5d19c1eaafcf016b99e88
= <=>
2021-08-26 09:07:26 +0100

    Updating flash script and firmware readme

--- README.md
+++ README.md
@@ -61,7 +61,7 @@ esptool.py -p $AMPY_PORT --baud 115200 write_flash --flash_size=detect 0 firmwar
 
 **Chip esp32**
 ```
-esptool.py --chip esp32 --port $AMPY_PORT --baud 460800 write_flash -z 0x1000 firmware/esp32-20190818-v1.11-219-gaf5c998f3.bin  
+esptool.py --chip esp32 --port $AMPY_PORT --baud 460800 write_flash -z 0x1000 firmware/esp32-20210623-v1.16.bin 
 ```
 
 ## Connect to the device
--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 !#bin/bash
-
-ampy -p $PORT put src/config.py config.py
-ampy -p $PORT put lib/copy.py copy.py
-ampy -p $PORT put lib/types.py types.py
-ampy -p $PORT put src/main.py main.py
+echo "Erasing flash"
+esptool.py -p $AMPY_PORT erase_flash
+sleep 1
+echo "Flashing firmware"
+esptool.py --chip esp32 --port $AMPY_PORT --baud 460800 write_flash -z 0x1000 firmware/esp32-20210623-v1.16.bin
+sleep 1
+echo "Uploading lantern library"
+ampy put lantern/
+sleep 1
+echo "Uploading config"
+ampy put local_config.json
+sleep 1
+echo "Uploading main script"
+ampy put main.py
+echo "All done!"

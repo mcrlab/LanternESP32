eb6c6db84f08acc3875a02c52729b979a51315e1
= <=>
2021-08-30 20:45:43 +0100

    Network config for update script

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from lantern.config_provider import ConfigProvider
 from lantern.bootstrap import start
 
 
-def download_and_install_update_if_available(updater):
-    updater.download_and_install_update_if_available('PLUSNET-HHQJC9', 'f6db6dd64c')
+def download_and_install_update_if_available(updater, ssid, password):
+    updater.download_and_install_update_if_available(ssid, password)
   
 def boot():
     provider = ConfigProvider()
+    network_config = provider.network_config
     updater = OTAUpdater('https://github.com/mcrlab/LanternIoT', main_dir='lantern')
-    download_and_install_update_if_available(updater)
+    download_and_install_update_if_available(updater, network_config['ssid'], network_config['password'])
     start(updater, provider)
 
 boot()

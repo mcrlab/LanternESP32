84163d561fc6e8de25e7ba30d338af9792bc9bbe
= <=>
2022-03-21 20:38:46 +0000

    adding in provisioning script and updated firmware

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,38 @@
-!#bin/bash
+#!/bin/bash
+counter=0
+
+while [ ! -e /dev/cu.usbmodem01 ]; do
+    sleep 1
+    counter=$((counter + 1))
+    echo "Waiting...."
+    if [ $counter -ge 50 ]; then
+        echo "Device not found"
+        exit
+    fi
+done
+
+
+esptool.py --chip esp32s2 --port /dev/cu.usbmodem01 --after no_reset erase_flash
+sleep 1
+esptool.py --chip esp32s2 --port /dev/cu.usbmodem01 --after no_reset write_flash -z 0x1000 ./firmware/LOLIN_S2_MINI-20220117-v1.18.bin 
+sleep 1
+
+echo "Press reset button on esp32"
+
+counter=0
+
+while [ ! -e /dev/cu.usbmodem1234561 ]; do
+    sleep 1
+    counter=$((counter + 1))
+    echo "Waiting for reset..."
+    if [ $counter -ge 50 ]; then
+        exit
+    fi
+done
+
 echo "Upoading mqtt libs"
 ampy put lib
-sleep 1     
+sleep 1
 echo "Uploading lantern library"
 ampy put lantern/
 sleep 1
@@ -14,3 +45,4 @@ sleep 1
 echo "Uploading main script"
 ampy put main.py
 echo "All done!"
+exit 0

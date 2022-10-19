afe498c7a1105e8983c390197e5bc4e0df1fcc34
= <=>
2022-10-19 21:02:09 +0100

    proxy for OTA updates

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-try:
-    from lantern.ota_updater import OTAUpdater
-except (ModuleNotFoundError, ImportError) as e:
-    from mocks import OTAUpdater
-
+from lantern.ota_updater import OTAUpdater
 from lantern.app import App
 
 def run():
-    updater = OTAUpdater('https://github.com/mcrlab/LanternESP32', main_dir='lantern')
+    updater = OTAUpdater('https://github.com/mcrlab/LanternESP32', 
+main_dir='lantern', proxy="http://pi4.local")
     app = App(updater)
     app.main()
 
